253e93423b93b6cdbac03f0e88ede1e41f7e7b51
Liz Kinker <Liz.Kinker@gmail.com>
2023-08-25 23:00:29 -0400

    Not sure if that's correct, but it works.

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -2,7 +2,57 @@ package com.zipcodewilmington.looplabs;
 
 /**
  * Created by leon on 1/29/18.
+ *
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    public int countOccurences(Integer value) {
+        int count = 0;
+        for(Integer i : array){
+            if(i==value) count++;
+        }
+        return count;
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int newArraySize = 0;
+        for(Integer i : array){
+            if(countOccurences(i)<maxNumberOfDuplications) newArraySize++;
+        }
+        Integer[] newArray = new Integer[newArraySize];
+
+        int newIndex = 0;
+        for(Integer i : array){
+            if(countOccurences(i)<maxNumberOfDuplications){
+                newArray[newIndex]=i;
+                newIndex++;
+            }
+        }
+
+        return newArray;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int newArraySize = 0;
+        for(Integer i : array){
+            if(countOccurences(i)!=exactNumberOfDuplications) newArraySize++;
+        }
+        Integer[] newArray = new Integer[newArraySize];
+
+        int newIndex = 0;
+        for(Integer i : array){
+            if(countOccurences(i)!=exactNumberOfDuplications){
+                newArray[newIndex]=i;
+                newIndex++;
+            }
+        }
+
+        return newArray;
+    }
 }
